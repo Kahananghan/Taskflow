a031be4ab0b17cb0c7628ef47223a63f9a8807ed
Kahananghan <kahanak1205@gmail.com>
2025-07-23 21:18:42 +0530

    nextauth

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth"
 import CredentialsProvider from "next-auth/providers/credentials";
+import GoogleProvider from "next-auth/providers/google";
 
 const handler = NextAuth({
     providers: [
@@ -23,6 +24,10 @@ const handler = NextAuth({
             }
         }),
 
+        GoogleProvider({
+         clientId: "process.env.GOOGLE_CLIENT_ID",
+         clientSecret: "process.env.GOOGLE_CLIENT_SECRET"
+  })
     ],
     
 })
